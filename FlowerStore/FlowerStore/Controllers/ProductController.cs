5ec344ea33e5f3c2134459dd12e77d4f69ba00b1
iftikharbg <iftikharbg@code.edu.az>
2021-12-10 23:51:41 +0400

    Homework

---
+++ FlowerStore/FlowerStore/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using FlowerStore.DAL;
+using FlowerStore.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlowerStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private AppDbContext _context;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public PartialViewResult LoadMore(int skipCount)
+        {
+            var product = _context.Products.Skip(skipCount).Take(4).Include(p => p.Category).ToList();
+            return PartialView("_ProductPartial", product);
+        }
+        
+        public IActionResult Index()
+        {
+            var product = _context.Products.Take(4).Include(a=>a.Category).ToList();
+            ViewBag.SkipCount = product.Count;
+            return View(new ProductViewModel {product=product });
+        }
+    }
+}
